634d86631508abe3921bd2ea37866c3996ca11f1
ainnGit <adriano0706@wp.pl>
2016-04-26 16:31:30 +0200

    Add third iCal

--- iCalProject/src/iCalProject/Starter.java
+++ iCalProject/src/iCalProject/Starter.java
@@ -10,11 +10,15 @@ public class Starter {
 	public Starter(){
 		iCal cal = new iCal("Pierwszy", 1);
 		iCal cal2 = new iCal("Drugi", 2);
+		iCal cal3 = new iCal("Trzeci", 3);
 		
 		System.out.println("Pierwszy iCal.");
 		cal.desc();
 		
 		System.out.println("Drugi iCal");
 		cal2.desc();
+		
+		System.out.println("Trzeci iCal");
+		cal3.desc();
 	}
 }
