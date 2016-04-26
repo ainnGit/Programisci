3a0474603c9477102070550bbc247fe7e206936a
ainnGit <adriano0706@wp.pl>
2016-04-26 16:27:57 +0200

    Add second iCal

--- iCalProject/src/iCalProject/Starter.java
+++ iCalProject/src/iCalProject/Starter.java
@@ -3,14 +3,18 @@ package iCalProject;
 public class Starter {
 
 	public static void main(String[] args) {
-		
+		// TODO Auto-generated method stub
 		new Starter();
 	}
 	
 	public Starter(){
 		iCal cal = new iCal("Pierwszy", 1);
+		iCal cal2 = new iCal("Drugi", 2);
 		
 		System.out.println("Pierwszy iCal.");
 		cal.desc();
+		
+		System.out.println("Drugi iCal");
+		cal2.desc();
 	}
 }
